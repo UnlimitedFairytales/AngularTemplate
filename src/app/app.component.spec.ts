923c018e502213e18056f0a2e348ea0bbc1ce9d7
DESKTOP-BVING6D\Ether <etherealgarden@msn.com>
2023-02-28 20:03:22 +0900

    初期サンプルページの内容削除

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -25,11 +25,4 @@ describe('AppComponent', () => {
     const app = fixture.componentInstance;
     expect(app.title).toEqual('project-template');
   });
-
-  it('should render title', () => {
-    const fixture = TestBed.createComponent(AppComponent);
-    fixture.detectChanges();
-    const compiled = fixture.nativeElement as HTMLElement;
-    expect(compiled.querySelector('.content span')?.textContent).toContain('project-template app is running!');
-  });
 });
